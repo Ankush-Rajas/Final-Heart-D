d757349842ae94c15bc26cf5f26f9dd4cd5eb34b
Ankush-Rajas <rajasankush1070@gmail.com>
2026-02-01 13:41:41 +0530

    Update save_report.py

--- utils/save_report.py
+++ utils/save_report.py
@@ -1,32 +1,59 @@
-import mysql.connector
-import os
-from utils.db import get_db_connection
+# import mysql.connector
+# import os
+# from utils.db import get_db_connection
 
-def save_report_to_db(patient_id, patient_name, risk, pdf_bytes):
+# def save_report_to_db(patient_id, patient_name, risk, pdf_bytes):
+#     conn = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="0103",
+#         database="heart_disease_db"
+#     )
+
+#     cursor = conn.cursor()
+
+#     sql = """
+#     INSERT INTO patient_reports
+#     (patient_id, patient_name, risk_level, report_blob)
+#     VALUES (%s, %s, %s, %s)
+#     """
+
+#     cursor.execute(sql, (
+#         patient_id,
+#         patient_name,
+#         risk,
+#         pdf_bytes
+#     ))
+
+#     conn.commit()
+#     print("✅ Report saved in DB")
+
+#     cursor.close()
+#     conn.close()
+
+
+import mysql.connector
+import streamlit as st
+
+def save_report_to_db(patient_id, patient_name, risk_level, pdf_bytes):
     conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="0103",
-        database="heart_disease_db"
+        host=st.secrets["MYSQLHOST"],
+        user=st.secrets["MYSQLUSER"],
+        password=st.secrets["MYSQLPASSWORD"],
+        database=st.secrets["MYSQLDATABASE"],
+        port=st.secrets["MYSQLPORT"]
     )
 
     cursor = conn.cursor()
-
-    sql = """
-    INSERT INTO patient_reports
-    (patient_id, patient_name, risk_level, report_blob)
-    VALUES (%s, %s, %s, %s)
-    """
-
-    cursor.execute(sql, (
-        patient_id,
-        patient_name,
-        risk,
-        pdf_bytes
-    ))
+    cursor.execute(
+        """
+        INSERT INTO patient_reports
+        (patient_id, patient_name, risk_level, report_blob)
+        VALUES (%s, %s, %s, %s)
+        """,
+        (patient_id, patient_name, risk_level, pdf_bytes)
+    )
 
     conn.commit()
-    print("✅ Report saved in DB")
-
     cursor.close()
     conn.close()
